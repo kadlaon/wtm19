df12aed8dd50df17ccf391031457963c26e80c18
Kristine Mae M. Adlaon <kristine_adlaon@dlsu.edu.ph>
2019-10-10 23:56:43 +0800

    Update bigram.py

--- bigram.py
+++ bigram.py
@@ -1,1 +1,58 @@
+import re
+import math
+from unigram import unigram_frequencies
+
+# sentence start and end
+sentence_start = "<s>"
+sentence_end = "</s>"
+bigram_frequencies = dict()
+bigram_word_probabilities = dict()
+
+def read_sentences_from_file(file_path):
+    with open(file_path, "r") as f:
+        return [re.split("\s+", line.rstrip('\n')) for line in f]
+
+def Bigram(sentences):
+	
+	unique_bigrams = set()
+	for sentence in sentences:
+		previous_word = None
+		for word in sentence:
+			if previous_word != None:
+				bigram_frequencies[(previous_word, word)] = bigram_frequencies.get((previous_word, word),0) + 1
+			if previous_word != sentence_start and word != sentence_end:
+				unique_bigrams.add((previous_word, word))
+			previous_word = word
+	#unique__bigram_words = len(unigram_frequencies)
+	return unique_bigrams, unique__bigram_words, bigram_frequencies, unigram_frequencies
+
+def compute_bigram_probabilities(previous_word, word):
+	bigram_word_probability_numerator = bigram_frequencies.get((previous_word, word), 0)
+	bigram_word_probability_denominator = unigram_frequencies.get(previous_word, 0)
+
+	return 0.0 if bigram_word_probability_numerator == 0 or bigram_word_probability_denominator == 0 else float(
+            bigram_word_probability_numerator) / float(bigram_word_probability_denominator)
+
+def main():
+	data = read_sentences_from_file("./data.txt")
+	test_data = read_sentences_from_file("./test_data.txt")
+
+	print("==============Bigram===============================")
+	a,b,c,d = Bigram(toy_dataset_test)
+	print("Unique Bigrams;",a)
+	print("Bigram Frequencies:",c)
+	print(d)		
+	for sentence in toy_dataset_test:
+		previous_word = None
+		for word in sentence:
+			if previous_word != None:
+				bi_prob = compute_bigram_probabilities(previous_word, word)
+				bigram_word_probabilities[(previous_word, word)] = bigram_word_probabilities.get((previous_word, word),bi_prob)
+			previous_word = word
+
+	for key,val in bigram_word_probabilities.items():
+		print(key,val)
+	
+	
+main()   
 
